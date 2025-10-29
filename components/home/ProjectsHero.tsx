a1db43ace371efcbbf42a7608b19fc58a30f7844
Dario George <edu.dariogeorge21@gmail.com>
2025-10-29 14:45:09 +0530

    update ProjectsHero image source for improved visual consistency

--- components/home/ProjectsHero.tsx
+++ components/home/ProjectsHero.tsx
@@ -34,7 +34,7 @@ export default function ProjectsHero() {
       >
         <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-black/40 to-black/60 z-10" />
         <img
-          src="https://images.pexels.com/photos/3862130/pexels-photo-3862130.jpeg?auto=compress&cs=tinysrgb&w=1920"
+          src="https://images.pexels.com/photos/51325/industry-sunset-port-facility-mood-51325.jpeg?auto=compress&cs=tinysrgb&w=1920"
           alt="Construction projects"
           className="w-full h-full object-cover"
         />
