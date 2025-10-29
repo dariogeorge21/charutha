4dd01793b55911f4c182f5ce7a7e4553830c34f7
Dario George <edu.dariogeorge21@gmail.com>
2025-10-29 14:59:05 +0530

    adjust gradient overlay position in ProjectsHero for improved visual effect

--- components/home/ProjectsHero.tsx
+++ components/home/ProjectsHero.tsx
@@ -32,7 +32,7 @@ export default function ProjectsHero() {
           willChange: 'transform',
         }}
       >
-        <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-black/40 to-black/60 z-10" />
+        <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-black/40 to-black/60 z-10 -mt-10" />
         <img
           src="https://images.pexels.com/photos/51325/industry-sunset-port-facility-mood-51325.jpeg?auto=compress&cs=tinysrgb&w=1920"
           alt="Construction projects"
